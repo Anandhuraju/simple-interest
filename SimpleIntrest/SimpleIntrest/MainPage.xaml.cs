035a69ca7884a5dfb7ce8d69d74421d7f6d4074f
Anandhuraju <77533028+Anandhuraju@users.noreply.github.com>
2022-02-12 12:25:07 +0530

    Add files via upload

---
+++ SimpleIntrest/SimpleIntrest/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleIntrest
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();            
+
+        }
+
+        private void Calculate_Clicked(object sender, EventArgs e)
+        {
+            float Intreast, TAmnt, year, IAmnt,total;
+            Intreast = Convert.ToInt32(Intrest.Text);
+            IAmnt = Convert.ToInt32(Amount.Text);
+            year = Convert.ToInt32(Year.Text);
+          
+            TAmnt = (Intreast * IAmnt * year) /100;
+            IAmount.Text = "₹" + Convert.ToString(TAmnt);
+            total = TAmnt + IAmnt;
+            TOTAL.Text = "₹" + Convert.ToString(total);
+        }
+
+        private void Clear_Clicked(object sender, EventArgs e)
+        {
+            Intrest.Text = "";
+            Amount.Text = "";
+            Year.Text = "";
+            IAmount.Text = "";
+            TOTAL.Text = "";
+        }
+    }
+}
